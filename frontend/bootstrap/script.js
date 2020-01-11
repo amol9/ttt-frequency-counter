b596cee91532949d5d9f9f89004c6226052c6981
Amol <babaiscool@gmail.com>
2020-01-11 09:26:59 +0530

    working frontend

--- frontend/bootstrap/script.js
+++ frontend/bootstrap/script.js
@@ -1,28 +1,18 @@
-console.log("script file")
+$(document).ready(function() {
+    $('form').submit(function() {
+        var formData = {
+            n: $('input[name=n]').val()
+        }
 
-function formSubmit() {
-    console.log("form submit fn")
-    var n = document.getElementById("n").value;
-    // Returns successful data submission message when the entered information is stored in database.
-    var dataString = 'n=' + n;
-
-    if (n == '') {
-        alert("Please Fill All Fields");
-    } else {
-        // AJAX code to submit form.
-        console.log("posting")
         $.ajax({
-            type: "POST",
-            //url: "http://localhost:8000/",
-            url: "https://jsonplaceholder.typicode.com/todos/1",
-            crossDomain: true,
-            data: dataString,
-            cache: false,
-            success: function (html) {
-                console.log(dataString)
-                alert(html);
-            }
-        });
-    }
-    return false;
-}
+            url: "http://localhost:8000/topN",
+            type: 'post',
+            data: formData,
+            dataType: 'json',
+            encode: true
+        }).done(data => {
+            console.log(data)
+        })
+        event.preventDefault()
+    })
+})
