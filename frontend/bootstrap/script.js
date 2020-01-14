763467bccf7ba7e581a40fc8bb2c3c70af3c6892
Amol <babaiscool@gmail.com>
2020-01-14 10:16:18 +0530

    more frontend stuff

--- frontend/bootstrap/script.js
+++ frontend/bootstrap/script.js
@@ -1,5 +1,8 @@
-$(document).ready(function() {
-    $('form').submit(function() {
+$(document).ready(function () {
+    var resultDiv = document.getElementById('result-div')
+    resultDiv.hidden = true
+
+    $('form').submit(function () {
         var formData = {
             n: $('input[name=n]').val()
         }
@@ -12,6 +15,22 @@ $(document).ready(function() {
             encode: true
         }).done(data => {
             console.log(data)
+            var table = document.getElementById('result')
+            var tbody = table.tBodies[0]
+
+            var newTbody = document.createElement('tbody')
+
+            data.top.forEach(item => {
+                var row = newTbody.insertRow()
+                var td = row.insertCell(0)
+                td.innerHTML = item[0]
+                var td2 = row.insertCell(1)
+                td2.className = "d-flex justify-content-end"
+                td2.innerHTML = item[1]
+            })
+
+            table.replaceChild(newTbody, tbody)
+            resultDiv.hidden = false
         })
         event.preventDefault()
     })
